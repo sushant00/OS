af71b9d50ca8b890f94fb8215721e320b784cf9b
nilay77 <nilay16063@iiitd.ac.in>
2018-02-11 13:27:59 +0530

    Test files addded

--- Assignment1/test1.c
+++ Assignment1/test1.c
@@ -8,8 +8,8 @@
 int main(int argc,char **argv)
 {
 	int pid=atoi(argv[1]);
-	printf("System call should return 0 and there should be no error\n");
-	long res=syscall(sys_sh_task_info,pid,"abc.txt");
+	long res=syscall(sys_sh_task_info,pid,"output.txt");
+	printf("System calll should return -1 and error should be file does not exist\n");
 	printf("System call returned %ld\n",res);
 	printf("Error: %s\n",strerror(errno));
 	return 0;
--- Assignment1/test2.c
+++ Assignment1/test2.c
@@ -8,7 +8,7 @@
 int main(int argc,char **argv)
 {
 	int pid=atoi(argv[1]);
-	printf("System call should return 0 and there should be no error\n");
+	printf("System call should return -1 and error should be no such process\n");
 	long res=syscall(sys_sh_task_info,pid,"abc.txt");
 	printf("System call returned %ld\n",res);
 	printf("Error: %s\n",strerror(errno));
