2d83c5c84355cbad3d945371b82785c4e505fbba
sushant <sushant2964@gmail.com>
2018-03-21 09:39:10 +0530

    test_rtnice

--- Assignment2/test_rtnice.c
+++ Assignment2/test_rtnice.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define rtnice 323
 
@@ -16,13 +17,13 @@ int main(int argc,char **argv)
 		
 		clock_t startTime = clock();
 		
-		long loopSize = 5e9;
+		long loopSize = 5e6;
 		long loop = 0;
 		for(loop = 0; loop < loopSize; loop++){			
 		}
 		double ex_time = ((double)(clock() - startTime))/CLOCKS_PER_SEC;
 
-		printf("child looped %ld times, took time ", loopSize, ex_time);
+		printf("child looped %ld times, took time %lf \n", loopSize, ex_time);
 		
 		exit(EXIT_SUCCESS);
 
@@ -34,19 +35,19 @@ int main(int argc,char **argv)
 		//successfully forked: parent process
 		wait(0);
 
-		// long res=syscall(rtnice,pid ,10000);
-		// printf("System call returned %ld\n",res);
-		// printf("Error: %s",strerror(errno));
+		 long res=syscall(323,pid ,10000);
+		 printf("System call returned %ld\n",res);
+		 printf("Error: %s \n",strerror(errno));
 
 		clock_t startTime = clock();
 		
-		long loopSize = 5e9;
+		long loopSize = 5e6;
 		long loop = 0;
 		for(loop = 0; loop < loopSize; loop++){			
 		}
 		double ex_time = ((double)(clock() - startTime))/CLOCKS_PER_SEC;
 
-		printf("parent looped %ld times, took time ", loopSize, ex_time);
+		printf("parent looped %ld times, took time %lf \n", loopSize, ex_time);
 		
 
 	}
